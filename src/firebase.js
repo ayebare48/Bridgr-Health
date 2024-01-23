5b6548bab8e4ffc916eaa780fe8ee1cd77fd3263
ayebare48 <113358924+ayebare48@users.noreply.github.com>
2024-01-23 09:55:31 +0300

    new push

---
+++ src/firebase.js
@@ -0,0 +1,65 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import {
+  getMessaging,
+  getToken,
+  onMessage,
+  isSupported,
+} from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfYG3pK7QZidj8kTlhjSjZ_qlMtIGdXCU",
+  authDomain: "bridgr-de3d9.firebaseapp.com",
+  projectId: "bridgr-de3d9",
+  storageBucket: "bridgr-de3d9.appspot.com",
+  messagingSenderId: "146837395809",
+  appId: "1:146837395809:web:be16901c69795665748d20",
+  measurementId: "G-M1DGGY3K5L",
+};
+const firebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
+const messaging = (async () => {
+  try {
+    const isSupportedBrowser = await isSupported();
+    if (isSupportedBrowser) {
+      return getMessaging(firebaseApp);
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
+})();
+
+export const fetchToken = async (setTokenFound, setFcmToken) => {
+  return getToken(await messaging, {
+    vapidKey:
+      "",
+  })
+    .then((currentToken) => {
+      if (currentToken) {
+        setTokenFound(true);
+        setFcmToken(currentToken);
+
+        // Track the token -> client mapping, by sending to backend server
+        // show on the UI that permission is secured
+      } else {
+        setTokenFound(false);
+        setFcmToken();
+        // shows on the UI that permission is required
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      // catch error while creating client token
+    });
+};
+
+export const onMessageListener = async () =>
+  new Promise((resolve) =>
+    (async () => {
+      const messagingResolve = await messaging;
+      onMessage(messagingResolve, (payload) => {
+        resolve(payload);
+      });
+    })()
+  );
